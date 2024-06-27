faf1100f9fd98130d25bb212bb617ab1d944c5c5
Alive <7116499@gmail.com>
2024-06-27 15:45:04 +0300

    Filter Screen

--- app/src/main/java/com/pavel/m_banking/ui/allTransaction/ShowTitleAllTransaction.kt
+++ app/src/main/java/com/pavel/m_banking/ui/allTransaction/ShowTitleAllTransaction.kt
@@ -1,29 +1,37 @@
 package com.pavel.m_banking.ui.allTransaction
 
-import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.pavel.m_banking.R
 import com.pavel.m_banking.navigation.ACCOUNT_SCREEN
+import com.pavel.m_banking.ui.allTransaction.filterByDate.FilterByDateScreen
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ShowTitleAllTransaction(navigationController: NavHostController) {
-    val ctx = LocalContext.current
+fun ShowTitleAllTransaction(navigationController: NavHostController,viewModel: AllTransactionViewModel) {
+    val sheetState = rememberModalBottomSheetState()
+    var showBottomSheet by remember { mutableStateOf(false) }
 
     Box {
         Image(
@@ -34,9 +42,7 @@ fun ShowTitleAllTransaction(navigationController: NavHostController) {
                 .align(Alignment.TopEnd)
                 .padding(top = 25.dp, end = 12.dp)
                 .clickable {
-                    Toast
-                        .makeText(ctx, "click", Toast.LENGTH_LONG)
-                        .show()
+                    showBottomSheet = true
                 }
         )
 
@@ -64,4 +70,16 @@ fun ShowTitleAllTransaction(navigationController: NavHostController) {
             )
         }
     }
+    if (showBottomSheet) {
+        ModalBottomSheet(
+            onDismissRequest = {
+                showBottomSheet = false
+            },
+            sheetState = sheetState,
+            containerColor = Color.Black
+        ) {
+            FilterByDateScreen(viewModel)
+        }
+    }
 }
+
